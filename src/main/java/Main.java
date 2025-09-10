9eea32fa13286df00100d2e790632a48e6c3d84d
0x0xoscar <139160108+0x0xoscar@users.noreply.github.com.>
2025-09-10 18:22:19 +0200

    Added append method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.printHead();
+        myLinkedList.printTail();
+        myLinkedList.printLength();
+
+        myLinkedList.printList();
     }
 }
