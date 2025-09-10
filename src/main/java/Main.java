062b3b65abf51f97e11006a4571a456e9bd08380
0x0xoscar <139160108+0x0xoscar@users.noreply.github.com.>
2025-09-10 18:43:27 +0200

    Added method to remove last node

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(4);
-
-        myLinkedList.printHead();
-        myLinkedList.printTail();
-        myLinkedList.printLength();
-
+        myLinkedList.append(2);
         myLinkedList.printList();
+
     }
 }
